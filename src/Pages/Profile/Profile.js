1a7703229f0791c9bda9d5e43dc6b8354ccba037
TusharGit <tusharmaheshwari29@gmail.com>
2023-06-06 10:38:23 +0530

    profile page updated

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,34 +1,214 @@
-import React, { useEffect, useState } from 'react';
-import {Formik} from 'formik';
-import  { Input, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { Formik } from "formik";
+import { Input, Grid, Typography, Box, Avatar, Button } from "@mui/material";
+import { LoginUserProfileDetails } from "../ReactQuery/CustomHooks/LeavePlanner";
+import {
+  KeyboardArrowLeft,
+  KeyboardArrowLeftOutlined,
+} from "@mui/icons-material";
+import Loader from "../Loader/Loader";
+import { useNavigate } from "react-router-dom";
 
 function ProfileComponent() {
+  const userValues = localStorage.getItem("value");
+  const finalUserValues = JSON.parse(userValues);
+  console.log(finalUserValues, "finalUserValues");
 
-    const userValues = localStorage.getItem('value')
-    const finalUserValues = JSON.parse(userValues)
+  const navigate = useNavigate();
 
-    const dashboardParent = {
-        backgroundColor: "#26b78a1c",
-        height: "545px",
-        width: "95%",
-        margin: "20px 0 0 31px",
-        borderRadius: "7px",
-      };
+  const { data, refetch, isFetching } = LoginUserProfileDetails(
+    finalUserValues?._id
+  );
+  const profileDetails = data?.data?.data;
+  const createdAtDate = new Date(profileDetails?.createdAt);
 
-      const textFields = {
-        display: "flex",
-        flexDirection: "column",
-        marginLeft: "104px",
-      };
+  useEffect(() => {
+    refetch();
+  }, []);
+  return (
+    <>
+      {isFetching ? <Loader /> : ""}
+      <Box
+        sx={{
+          background:
+            " linear-gradient(to left top,  rgba(51, 51, 51, 1) 0%,  rgb(0 0 0 / 90%)  ,rgba(85 173 136), rgba(51, 51, 51, 1)   100% )",
+          // background:
+          //   "linear-gradient(to right bottom, rgb(81, 155, 99), rgba(0, 0, 0, 0.95))",
+          width: "100vw",
+          height: "90vh",
+          marginTop: "10px",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "#fff",
+            width: "30%",
 
-      const inputFieldsWidth = {
-          width : '45%'
-      }
+            borderRadius: "8px",
+            p: "30px 20px",
+            boxShadow: "0px 0px 2px 0px",
+          }}
+        >
+          <Box
+            sx={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <Box>
+              <Avatar
+                sx={{ width: 80, height: 80, cursor: "pointer" }}
+                src="/broken-image.jpg"
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              margin: "20px 10px 0px",
+              display: "flex",
+              justifyContent: "space-between",
+              boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              borderRadius: "8px",
+              padding: "15px 8px",
+            }}
+          >
+            <Typography sx={{ color: "#726f6f" }}>Your User Name</Typography>
+            <Typography>
+              {profileDetails?.firstName} {profileDetails?.lastName}
+            </Typography>
+          </Box>
 
-    return (
-        <>  
+          <Box
+            sx={{
+              margin: "10px 10px 0px",
+              display: "flex",
+              justifyContent: "space-between",
+              boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              borderRadius: "8px",
+              padding: "15px 8px",
+            }}
+          >
+            <Typography sx={{ color: "#726f6f" }}>Your E-mail</Typography>
+            <Typography>{profileDetails?.email}</Typography>
+          </Box>
 
-        <Grid sx={dashboardParent}>
+          <Box
+            sx={{
+              margin: "10px 10px 0px",
+              display: "flex",
+              justifyContent: "space-between",
+              boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              borderRadius: "8px",
+              padding: "15px 8px",
+            }}
+          >
+            <Typography sx={{ color: "#726f6f" }}>Your Phone</Typography>
+            <Typography>{profileDetails?.phoneNumber}</Typography>
+          </Box>
+
+          <Box
+            sx={{
+              margin: "10px 10px 0px",
+              display: "flex",
+              justifyContent: "space-between",
+              boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              borderRadius: "8px",
+              padding: "15px 8px",
+            }}
+          >
+            <Typography sx={{ color: "#726f6f" }}>Date of birth</Typography>
+            <Typography>{profileDetails?.birthDate}</Typography>
+          </Box>
+
+          <Box
+            sx={{
+              margin: "10px 10px 0px",
+              display: "flex",
+              justifyContent: "space-between",
+              boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              borderRadius: "8px",
+              padding: "15px 8px",
+            }}
+          >
+            <Typography sx={{ color: "#726f6f" }}>Member Since</Typography>
+            <Typography>{createdAtDate.toLocaleDateString()}</Typography>
+          </Box>
+
+          <Box
+            sx={{
+              margin: "10px 10px 20px",
+              display: "flex",
+              justifyContent: "space-between",
+              boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              borderRadius: "8px",
+              padding: "15px 8px",
+            }}
+          >
+            <Typography sx={{ color: "#726f6f" }}>Designation</Typography>
+            <Typography>{profileDetails?.designation}</Typography>
+          </Box>
+          <Box
+            sx={{ display: "flex", justifyContent: "center", width: "100%" }}
+          >
+            <Button
+              variant="contained"
+              onClick={() =>
+                navigate("/edit-profile", { state: { data: profileDetails } })
+              }
+            >
+              Edit Details
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      {/* <Box
+        sx={{
+          backgroundColor: "#ced5df",
+          width: "100vw",
+          height: "90vh",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            width: "40%",
+            height: "85%",
+            backgroundColor: "#fff",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            borderRadius: "8px",
+          }}
+        >
+          <Typography variant="h4" sx={{ fontSize: "28px" }}>
+            {finalUserValues.firstName} {finalUserValues.lastName}
+          </Typography>
+
+          <Typography variant="h4" sx={{ fontSize: "16px", color: "gray" }}>
+            {finalUserValues.email}
+          </Typography>
+
+          <Box>
+            <Avatar sx={{ width: 100, height: 100 }} src="/broken-image.jpg" />
+          </Box>
+          <Typography variant="h4" sx={{ fontSize: "20px", color: "gray" }}>
+            {finalUserValues.role}
+          </Typography>
+          <Typography variant="h4" sx={{ fontSize: "18px", color: "gray" }}>
+            Birth Year :-{" "}
+            <Box component="span" sx={{ color: "black" }}>
+              {birthdateYear} {months[+birthDateMonth]} {birthDate}
+            </Box>
+          </Typography>
+
+          <Button variant="contained">Edit User</Button>
+        </Box>
+      </Box> */}
+
+      {/* =========================================================================================== */}
+
+      {/* <Grid sx={dashboardParent}>
         
         <Grid>
           <Typography variant="h5" style={{fontSize: '4rem', padding: '20px 0 20px 30px'}}>User Information :- </Typography>
@@ -106,9 +286,9 @@ function ProfileComponent() {
                     }}
 
             </Formik>
-            </Grid>
-        </>
-    )
+            </Grid> */}
+    </>
+  );
 }
 
 export default ProfileComponent;
